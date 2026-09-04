592da2ea3715a293089d0b971dda8212f5a4d9fd
Saniya Seerwani <saniyaseerwani33@gmail.com>
2026-09-05 05:08:56 +0530

    Use environment variable for backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 export default API;
+
 export const analyzeResume = async (formData) => {
   return await API.post("/ai/analyze", formData, {
     headers: {
